92cb57d09c480ef5f560073209df4da10664629e
Mohamed Zayed <mohammedkhaled077@gmail.com>
2024-03-13 00:42:08 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const firstArg = process.argv[2];
-
-if (!isNaN(parseInt(firstArg))) {
-  console.log(`My number: ${parseInt(firstArg)}`);
-} else {
-  console.log('Not a number');
-}
