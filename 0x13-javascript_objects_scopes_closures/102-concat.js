0bed3ec35fbb648d6e91489827d06de656478bd5
Mohamed Zayed <mohammedkhaled077@gmail.com>
2024-03-13 00:08:17 +0200

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,20 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-if (process.argv.length !== 5) {
-  console.error('Usage: ./102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
-
-const [, , fileA, fileB, fileC] = process.argv;
-
-try {
-  const dataA = fs.readFileSync(fileA, 'utf8');
-  const dataB = fs.readFileSync(fileB, 'utf8');
-  const concatenatedData = `${dataA}${dataB}`;
-  fs.writeFileSync(fileC, concatenatedData);
-} catch (error) {
-  console.error(`Error: ${error.message}`);
-  process.exit(1);
-}
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
